f06ad68ab8c1f3dcbce19193696c07c5c8f88928
jesusduque4 <jesusduque4@gmail.com>
2023-09-25 21:55:21 -0600

    Se agrego DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule
   ],
   providers: [],
